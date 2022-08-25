6aa4e4fcdfbbd4e730aac8e5d6aba3df232fc344
Ina-Verena.Ludwig <ina.verena.ludwig@gmail.com>
2022-08-25 20:16:52 +0200

    commit index

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { clear } from "@testing-library/user-event/dist/clear";
-import Clock from "./components/Clock";
-import Button from "./components/Button";
-import Table from "./components/Table";
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Table />, document.getElementById("root"));
+
+root.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
